4d0fc8c290997a03285e3061bcabc808cc572439
JuanPablo <juanp.escobar@udea.edu.co>
2020-06-26 17:36:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ensayo_int/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.ensayo_int
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timerTask
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_splash)
+
+        val timer = Timer()
+        timer.schedule(timerTask { goTologinActivity() }, 2000)
+    }
+
+
+    private fun goTologinActivity() {
+        val intent = Intent(this, loginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
